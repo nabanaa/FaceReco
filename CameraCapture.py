410d2fa35852669259c384163661a89a3a7114ca
Piotr Mika <piotr.mika@gsuite.us.edu.pl>
2024-01-09 06:03:14 +0100

    :art: :bug: :white_check_mark:

--- CameraCapture.py
+++ CameraCapture.py
@@ -1,6 +1,8 @@
+#!/usr/bin/env python3
+
 import numpy as np
 import cv2
-# import os
+import os
 import tensorflow as tf
 from tensorflow import keras
 import matplotlib.pyplot as plt
@@ -22,10 +24,45 @@ def create_video_writer(video_cap, output_filename):
     return writer
 
 video_cap = cv2.VideoCapture(0)
-# change according to your operating system
-# also you need to make all the emotion directories in pic_dir
-pic_dir = '/home/piotrmika/Rok3/Python/pics/'
 
+# creating folders for faces
+# they are created in the folder that the program was run
+pic_dir = os.path.join(os.getcwd(), 'Faces')
+if not os.path.exists(pic_dir):
+    os.makedirs(pic_dir)
+labels = ["Ahegao", "Sad", "Happy", "Surprise", "Angry", "Neutral"]
+for label in labels:
+    dir = os.path.join(pic_dir, label)
+    if not os.path.exists(dir):
+        os.makedirs(dir)
+
+def list_files(path):
+    onlyfiles = [f for f in os.listdir(path) if os.path.isfile(os.path.join(path, f))]
+    return onlyfiles
+
+def save_file(folderName, f_im):
+    from string import ascii_letters as alphabet
+    from random import choice
+    while True:
+        fileName = ""
+        for i in range(6):
+            fileName+=choice(alphabet)
+        fileName+=".jpg"
+        print(fileName)
+        path = os.path.join(pic_dir, folderName)
+        files = list_files(path)
+        check = True
+        for file in files:
+            if file==fileName:
+                check=False
+                break
+        if check:
+            break      
+    cv2.imwrite(os.path.join(path, fileName), f_im)
+    print("Image saved!")
+
+# Load the cascade
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 while True:
     ret, frame = video_cap.read()
 
@@ -33,56 +70,51 @@ while True:
     if not ret:
         break
 
-    # Load the cascade
-    face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
-
     # Convert into grayscale
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     
     # Detect faces
     faces = face_cascade.detectMultiScale(gray, 1.1, 4)
-    if not np.any(faces):
-        continue
     # Draw rectangle around the faces
     # offset needed to get the full face
-    offset = 30
-    face = faces[0]
-    (x, y, w, h) = face
-    cv2.rectangle(frame, (x-offset, y-offset), (x+w+offset, y+h+offset), (255, 0, 0), 1)
-    
-    f_im = frame[y-offset:y+h+offset, x-offset:x+w+offset, :]
-    back_color = (255,255,255)
-    font_color = (0,0,0)
-    cv2.rectangle(frame, (0, 0), (640, 20), back_color, -1)
-    cv2.putText(frame, "Press (h)Happy, o(Ahegao), (a)Angry, (s)Surprise, (n)Neutral, (d)Sad, (q)uit", (12,15), cv2.QT_FONT_NORMAL, 0.5,font_color , 1)
+    anyFaces = np.any(faces)
+    if anyFaces:
+        offset = 35
+        face = faces[0]
+        (x, y, w, h) = face
+        xw = frame.shape[1]
+        yw = frame.shape[0]
+        P1 = (x-offset if x-offset >= 0 else 0, y-offset if y-offset >= 0 else 0)
+        P2 = (x+w+offset if x+w+offset <= xw else xw, y+h+offset if y+h+offset <= yw else yw)
+        cv2.rectangle(frame, P1, P2, (255, 0, 0), 1)
+        f_im = frame[y-offset:y+h+offset, x-offset:x+w+offset, :]
+        print(frame.shape)
+        back_color = (255,255,255)
+        font_color = (0,0,0)
+        cv2.rectangle(frame, (0, 0), (640, 20), back_color, -1)
+        cv2.putText(frame, "Press (h)Happy, o(Ahegao), (a)Angry, (s)Surprise, (n)Neutral, (d)Sad, (q)uit", (12,15), cv2.QT_FONT_NORMAL, 0.5,font_color , 1)
     cv2.imshow("Frame", frame)
 
     key = cv2.waitKey(1) & 0xFF
 
     # user interface
-    if key == ord('h'):
-        cv2.imwrite(pic_dir+"/Happy/"+str(t.time())+'.jpg', f_im)
-        print("Image saved!")
+    if key == ord('h') and f_im is not None:
+        save_file("Happy", f_im)
         
-    if key == ord('o'):
-        cv2.imwrite(pic_dir+"/Ahegao/"+str(t.time())+'.jpg', f_im)
-        print("Image saved!")
+    if key == ord('o') and f_im is not None:
+        save_file("Ahegao", f_im)
     
-    if key == ord('a'):
-        cv2.imwrite(pic_dir+"/Angry/"+str(t.time())+'.jpg', f_im)
-        print("Image saved!")
+    if key == ord('a') and f_im is not None:
+        save_file("Angry", f_im)
 
-    if key == ord('s'):
-        cv2.imwrite(pic_dir+"/Surprise/"+str(t.time())+'.jpg', f_im)
-        print("Image saved!")
+    if key == ord('s') and f_im is not None:
+        save_file("Surprise", f_im)
 
-    if key == ord('n'):
-        cv2.imwrite(pic_dir+"/Neutral/"+str(t.time())+'.jpg', f_im)
-        print("Image saved!")
+    if key == ord('n') and f_im is not None:
+        save_file("Neutral", f_im)
 
-    if key == ord('d'):
-        cv2.imwrite(pic_dir+"/Sad/"+str(t.time())+'.jpg', f_im)
-        print("Image saved!")
+    if key == ord('d') and f_im is not None:
+        save_file("Sad", f_im)
 
     # Check for the 'q' key to exit the loop
     elif key == ord('q'):
