58e28ef115970d3791a9b7b89c309a4c70bffa6f
Nestrion <adam.pospieszny12@gmail.com>
2024-01-28 13:40:46 +0100

    shared venv

--- venv/setup_venv.ps1
+++ venv/setup_venv.ps1
@@ -1,10 +1,11 @@
 # install virtualenv command
-pip install --uprgrade pip
+pip install --upgrade pip
 pip install -r "requirements.txt"
 # setup and activate the environment
-if (virtualenv --python=3.11.1 .env -and source .env/bin/activate)
+if (virtualenv --python=3.11.1 env -and source env/bin/activate)
 {
     pip install -r "../requirements.txt"
 }
-
+source env/bin/activate
+pip install --upgrade pip
  
--- venv/setup_venv.sh
+++ venv/setup_venv.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/bash
 
 # install virtualenv command
-pip install --uprgrade pip
+pip install --upgrade pip
 pip install -r "requirements.txt"
 # setup and activate the environment
-virtualenv --python=3.11.1 .env && source .env/bin/activate && pip install -r "../requirements.txt" 
+virtualenv --python=3.11.1 env && source env/bin/activate && pip install -r "../requirements.txt" 
+source env/bin/activate
+pip install --upgrade pip
