4323006d6e73c6cb74a463faface6f5d978bb1e1
Nestrion <adam.pospieszny12@gmail.com>
2024-01-28 13:27:43 +0100

    venv setup scripts fix3

--- venv/setup_venv.sh
+++ venv/setup_venv.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/bash
 
 # install virtualenv command
+pip install --uprgrade pip
 pip install -r "requirements.txt"
 # setup and activate the environment
 virtualenv --python=3.11.1 .env && source .env/bin/activate && pip install -r "../requirements.txt" 
