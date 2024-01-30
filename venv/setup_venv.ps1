5a3b22646068a34ea21abbba83d49f5e9ef1ea15
Nestrion <adam.pospieszny12@gmail.com>
2024-01-30 12:22:09 +0100

    fixed setup_venv for windows

--- venv/setup_venv.ps1
+++ venv/setup_venv.ps1
@@ -2,10 +2,10 @@
 pip install --upgrade pip
 pip install -r "requirements.txt"
 # setup and activate the environment
-if (virtualenv --python=3.11.1 env -and source env/bin/activate)
+if (virtualenv --python=3.11.1 env -and ./env/bin/activate)
 {
     pip install -r "../requirements.txt"
 }
-source env/bin/activate
+./env/bin/activate
 pip install --upgrade pip
  
